cebb318778603c5ae8a4a044930af18baac9578a
rgan <rajaram.ganeshan@gmail.com>
2012-01-07 14:18:15 -0500

    Filters within 10 micron limit

--- src/magphys_clj/core.clj
+++ src/magphys_clj/core.clj
@@ -13,7 +13,19 @@
 
 (defrecord Filter [name lambda_eff id])
 
-(defn read-file [file]
-	(map #(let [x (str/split % #"\t\s")] (Filter. (first x) (Float. (second x)) (Integer. (str/trim (nth x 2))))) (rest (str/split-lines (slurp file))))
+(defn make-filter [name lambda_eff id]
+	(Filter. name lambda_eff id)
 )
+
+(defn read-file [file] (str/split-lines (slurp file)))
+
+(defn read-filter-file [file]
+	(map #(let [x (str/split % #"\t\s")] (make-filter (first x) (Float. (second x)) (Integer. (str/trim (nth x 2))))) (rest (read-file file)))
+)
+
+;if lambda > 10 microns, contribution from SPs is negligible
+(defn filters-within-10micron-limit [filters redshift]
+	(filter #(let [lambda_rest (/ (:lambda_eff %) (+ 1.0 redshift))] (< lambda_rest 10)) filters)
+)
+
 
--- test/magphys_clj/test/core.clj
+++ test/magphys_clj/test/core.clj
@@ -1,6 +1,7 @@
 (ns magphys-clj.test.core
   (:use [magphys-clj.core])
-  (:use [clojure.test]))
+  (:use [clojure.test])
+)
 
 (defn float= [x y]
   (let [epsilon 0.00001
@@ -11,10 +12,18 @@
    (is (= 4.7619047619047614E-5 (cosmological_constant 70.0 0.3 0.7)))
 )
 
-(deftest test-read-file
-   (let [result (read-file "filters.dat")]
+(deftest test-read-filter-file
+   (let [result (read-filter-file "filters.dat")]
 	(is (= 20 (count result)))
 	(is (= "U" (:name (first result))))
 	(is (float= 0.346 (:lambda_eff (first result))))
 	(is (= 222 (:id (first result))))
 ))
+
+(deftest test-should-return-filters-within-10micron-limit
+	(is (= 1 (count (filters-within-10micron-limit (list (make-filter "U" 0.346 222)) 0.4600))))
+)
+
+(deftest test-should-not-return-filters-outside-10micron-limit
+	(is (= 0 (count (filters-within-10micron-limit (list (make-filter "SPIRE500" 500 174)) 0.4600))))
+)
